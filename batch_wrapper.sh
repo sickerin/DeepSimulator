d17963aaf0ceeebff82c9dfd0bec83193ed42121
Maung of group neugebauer <dpm43@c21n09.farnam.hpc.yale.internal>
2018-10-17 00:38:20 -0400

    made some changes

--- batch_wrapper.sh
+++ batch_wrapper.sh
@@ -6,12 +6,31 @@ while [[ -n $1 ]]; do
         case $1 in
                 -f | --foldername )       shift
                                                 FOLDER=$1
-                                                ;;                      
+                                                ;;               
+                -r | --runumber )         shift
+                                                RUN=$1
+                                                ;;
         esac
         shift
 done
 
 FOLDER_N=$(basename "$FOLDER")
+RUN_N=$(basename "$RUN")
 
-for %f in FOLDER_N do ./single_batch.sh -f %f
+mkdir -p $RUN_N
+
+# run single sh, then move them into the run folder
+for f in ${FOLDER_N}/*.fasta
+ do echo "here"
+ y=${f%.fasta}
+ echo "${y##*/}"
+#  echo "after"
+#  echo "${f}"
+ ./single_batch.sh -f "$f"
+ echo "${RUN_N}"
+#  echo "here"
+#  echo "${f%%.fasta}"
+#  echo "after"
+ mv ${y##*/} ${RUN_N} -f
+done
 
